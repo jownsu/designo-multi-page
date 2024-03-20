db16d70ec30c73e59eb0385b53e78e127a6fe64d
Alfonso Martin Angeles <alfonsomartinangeles@gmail.com>
2024-03-20 11:32:12 +0800

    Add Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Jost } from "next/font/google";
 import "./globals.css";
+import Navbar from "./Navbar";
 
 const jost = Jost({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
 	return (
 		<html lang="en"> 
 			<body className={jost.className}>
+				<Navbar/>
 				<main className="container mx-auto">
 					{children}
 				</main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,26 @@
 import type { Config } from "tailwindcss";
-
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#E7816B",
-        primary_light: "#FFAD9B",
-		    black_1: "#FFAD9B",
-        dark_grey: "#333136",
-        light_grey: "#F3F4F6",
-      }
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}"
+    ],
+    theme: {
+        container: {
+            screens: {
+                lg: "1111px"
+            }
+        },
+        extend: {
+            colors: {
+                primary: "#E7816B",
+                primary_light: "#FFAD9B",
+                black_1: "#1D1C1E",
+                dark_grey: "#333136",
+                light_grey: "#F3F4F6"
+            }
+        }
     },
-  },
-  plugins: [],
+    plugins: []
 };
 export default config;
