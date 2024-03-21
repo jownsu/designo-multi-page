36b4dd533aeaa6ea942e83de30e1449219092953
Edmond Esquilon <esquilon.edmond@gmail.com>
2024-03-21 10:07:56 +0800

    added contact api

--- app/contact/_components/ContactForm.tsx
+++ app/contact/_components/ContactForm.tsx
@@ -1,8 +1,8 @@
 
 "use client";
-import { useForm } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { z } from "zod";
-import { form_validation } from "./forrmSchema";
+import { contact_schema } from "../../validationSchema";
 import { zodResolver } from '@hookform/resolvers/zod';
 import classNames from "classnames";
 import * as Form from "@radix-ui/react-form";
@@ -10,53 +10,62 @@ import BGCirclePatternDesktop from "@/public/images/contact/desktop/bg-pattern-h
 import BGCirclePatternMobile from "@/public/images/contact/mobile/bg-pattern-hero-contact-mobile.svg";
 import Button from "@/app/_components/Button";
 
-type ContactFormData = z.infer<typeof form_validation>;
+type ContactFormData = z.infer<typeof contact_schema>;
 
 const ContactForm = () => {
     const {register, control, handleSubmit, formState: { errors }} = useForm<ContactFormData>({
-        resolver: zodResolver(form_validation)
+        resolver: zodResolver(contact_schema)
     });
 
-    const handleOnSubmit = handleSubmit( async(data) => {
-        try {
-            //do data api submission here.
-            alert("form submitted!");
-            console.log(data);
+    // const handleOnSubmit = handleSubmit( async(data) => {
+    //     try {
+    //         const response = await fetch("/api/contact", {
+    //             method: "POST",
+    //             headers: {
+    //                 "Content-type": "application/json"
+    //             },
+    //             body: JSON.stringify(data)
+    //         });
+
+    //         if(!response.ok) throw new Error("There was an error in network response!");
             
-        } catch (error) {
-            //do error handling here.
-            console.log(errors);
+    //     } catch (error) {
+    //         //do error handling here.
+    //         console.log(errors);
             
-        }
-    });
+    //     }
+    // });
+
+   const onSubmit = () => alert("dqoidhqw");
   
     return (
-        <div className="text-white overflow-hidden bg-primary py-[72px] flex flex-col md:items-center lg:flex-row min-h-[375px] sm:rounded-lg sm:px-14 relative">
+        <div className="text-white overflow-hidden bg-primary py-[54px] flex flex-col md:items-center lg:flex-row min-h-[483px] sm:rounded-lg sm:px-14 relative">
             <BGCirclePatternDesktop className="fill-inherit z-0 absolute hidden sm:left-[-120px] sm:top-[-100px] lg:top-[-135px] lg:left-[-10px] sm:block"/>
             <BGCirclePatternMobile className="fill-inherit z-0 absolute sm:hidden top-[-1px] left-[-90px]"/> 
             <div className="flex flex-col text-center px-4 mb-12 md:text-left z-10">
                 <h1 className="text-5xl font-medium mb-8">Contact Us</h1>
                 <p className="text-base">Ready to take it to the next level? Let&apos;s talk about your project or idea and find out how we can help your business grow. If you are looking for unique digital experiences that&apos;s relatable to your users, drop us a line.</p>
             </div>
-            <Form.Root autoComplete="off" onSubmit={handleOnSubmit} className="flex md:min-w-[445px] flex-col gap-3 px-6 w-full z-10">
-                <Form.Field {...register("name")}>
+            <Form.Root autoComplete="off" onSubmit={onSubmit} className="flex md:min-w-[445px] flex-col gap-3 px-6 w-full z-10">
+                <Form.Field asChild {...register("name")} >
                     <input type="text" className={classNames({ input_fields: true })} placeholder="Name" />
                 </Form.Field>
-                <Form.Field {...register("email")} >
+                <Form.Field asChild {...register("email")} >
                     <input type="email" className={classNames({ input_fields: true })} placeholder="Email"/>
                 </Form.Field>
-                <Form.Field {...register("phone")} >
+                <Form.Field asChild {...register("phone")} >
                     <input type="tel" className={classNames({ input_fields: true })} placeholder="Phone"/>
                 </Form.Field>
-                <Form.Field {...register("message")}>
+                <Form.Field asChild {...register("message")} >
                     <textarea className={classNames({ input_fields: true, "resize-none":true, "overflow-hidden": true })} placeholder="Message"/>
                 </Form.Field>
-                <Form.Submit asChild>
+                <Form.Submit asChild >
                     <Button variant="secondary" className="md:ml-auto md:mx-0 mx-auto" >
                         SUBMIT
                     </Button>  
                 </Form.Submit>
             </Form.Root>
+       
         </div>
     )
 }
--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -1,6 +1,6 @@
 import {z} from "zod";
 
-export const form_validation = z.object({
+export const contact_schema = z.object({
     name: z.string().min(1, "Can't be empty").max(50).optional(),
     email: z.string().email().max(50),
     phone: z.number().max(12),
