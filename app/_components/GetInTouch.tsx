b0c30ee4f7e8844331e973a28950e20c3bb041be
jownsu <jhonesdigno666@gmail.com>
2024-03-22 11:00:08 +0800

    Added reusable slide

--- app/_components/GetInTouch.tsx
+++ app/_components/GetInTouch.tsx
@@ -1,9 +1,9 @@
 "use client";
 
+import { usePathname, useRouter } from "next/navigation";
+import { AttentionSeeker } from "react-awesome-reveal";
 import Button from "./Button";
-import { Slide, AttentionSeeker } from "react-awesome-reveal";
-import { usePathname } from "next/navigation";
-import { useRouter } from "next/navigation";
+import Slide from "./Slide";
 
 const GetInTouch = () => {
     const pathname = usePathname();
@@ -27,7 +27,12 @@ const GetInTouch = () => {
                     </p>
                 </div>
                 <AttentionSeeker effect="tada" delay={1000}>
-                    <Button variant="secondary" onClick={() => router.push("/contact")}>Get in touch</Button>
+                    <Button
+                        variant="secondary"
+                        onClick={() => router.push("/contact")}
+                    >
+                        Get in touch
+                    </Button>
                 </AttentionSeeker>
             </div>
         </Slide>
